780185f300b1e79a8844852e428569faaf86252b
DESKTOP-OS62DSG\User <cramirez@enfoco.com.do>
2021-02-18 09:59:24 -0400

    Primer commit-tamplate listo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { dashboardRoutes } from './dashboard/dashboard.routes';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login',   component: LoginComponent},
+  {path: 'regiter', component: RegisterComponent},
+  {
+    path: '',
+  component: DashboardComponent,
+  children: dashboardRoutes
+  },
+  {path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
